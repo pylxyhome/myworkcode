4e9fffbb9166a7377d3e178afa7219741fe60ce5
pylxyhome <pylxyhome@vip.qq.com>
2012-04-16 22:57:43 +0800

    push

--- webpush/PushStatusServlet.java
+++ webpush/PushStatusServlet.java
@@ -47,18 +47,6 @@ public class PushStatusServlet extends HttpServlet {
 		doPost(request, response);
 	}
 
-	private String getIpAddr(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		return ip;
-	}
+	
 
 }
